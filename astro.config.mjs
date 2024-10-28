796686a0aec7e71e454f8bd5112310f9108875a2
christopher coyne <christcoyne@gmail.com>
2024-10-27 22:45:06 -0400

    fix: add daisy ui + tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), icon()],
+  integrations: [mdx(), icon(), tailwind()]
 });
