07261532d1091547da9e6b542bc1385044382386
christophercoyne <christcoyne@gmail.com>
2026-08-27 15:43:13 -0400

    fix: remove spa mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [mdx(), icon(), tailwind({ applyBaseStyles: false }), react()]
 });
